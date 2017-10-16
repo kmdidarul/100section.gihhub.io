15d49385e363f5a55b0389cd37c195c5e46225fb
kmdidarul <didarul.gai@gmail.com>
2017-10-16 22:54:06 +0600

    class-87

---
+++ class-87/assets/js/main.js
@@ -0,0 +1,40 @@
+
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+
+
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: false,
+            dots: true,
+            nav: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            dotsData: true  
+            
+        });
+
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
